665792892dfa72fae282c5ad2011ba5786fb6f6c
Alex Gorman <alexgorman@Alexs-MacBook-Pro.local>
2025-08-01 20:03:49 -0400

    Refactored the adding, editing, and deleting of appointments from the MainMenu.js file, to the AppointmentFormPopup.js. Timesheet still correctly displays the appointments at their correct times with the correct info. Next job is to add date selection to appointment booking.

--- frontend/src/MainMenu/MainMenu.js
+++ frontend/src/MainMenu/MainMenu.js
@@ -7,42 +7,11 @@ import AppointmentFormPopup from '../Components/AppointmentFormPopup';
  * Includes EMR subbars, schedule time slots, and a form for posting data.
  */
 function MainMenu() {
-
-  /* Store the current time slot that was clicked for booking */
-  const [selectedTime, setSelectedTime] = useState(null);
-
-  /* Store and control whether the appt booking window is visible */
-  const [showBookingWindow, setShowBookingWindow] = useState(false);
-
-  /* Store the name of the pt entered in the booking window */
-  const [patientName, setPatientName] = useState('');
-
-  /* Store list of patient search matches returned from backend */
-  const [searchResults, setSearchResults] = useState([]);
-
-  /* Store patient selected from search results for booking */
-  const [selectedPatient, setSelectedPatient] = useState([]);
-
   /* Store the current date, HARDCODE FOR NOW */
   const currentDate = '2025-06-27'; /* REPLACE WITH DYANMIC DATE LATER */
 
   /* Store the list of appointments retrieved from the backend server to display to the timesheet */
   const [appointments, setAppointments] = useState([]);
-
-  /* Store the appointment duration time in minutes */
-  const [duration, setDuration] = useState(15); /* 15 min default */
-
-  /* Store the selected provider when booking an appt */
-  const [selectedProviderId, setSelectedProviderId] = useState(null);
-
-  /* Track the selected appointment */
-  const [selectedAppointment, setSelectedAppointment] = useState(null);
-
-  /* Track the appointment reason */
-  const [appointmentReason, setAppointmentReason] = useState('');
-
-  /* Popup window */
-  const [popupVisible, setPopupVisible] = useState(false);
 
   /* Take a message from the add appointment window to refresh the timesheet and grab all new appointments */
   useEffect(() => {
@@ -56,19 +25,6 @@ function MainMenu() {
       window.removeEventListener('message', handleMessage);
     };
   }, []);
-
-  
-  /* Reset booking state values */
-  const resetBookingState = () => {
-    setSelectedTime(null);
-    setShowBookingWindow(false);
-    setPatientName('');
-    setSearchResults([]);
-    setSelectedPatient(null);
-    setSelectedProviderId(null);
-    setSelectedAppointment(null);
-    setAppointmentReason('');
-  }
 
 
   /* Fetch the list of appointments whenever the date changes */
@@ -91,156 +47,12 @@ function MainMenu() {
    * Handle the user click of the time slot
    */
   const handleTimeClick = (time, providerId) => {
-    resetBookingState();
-    setSelectedTime(time);
-    setSelectedProviderId(providerId);
-    // setShowBookingWindow(true);
-    console.log("GOT HERE 1");
 
     /* Query string with the info to be passed to the popup window */
     const popupURL = `/appointment-form-popup?time=${encodeURIComponent(time)}&providerId=${providerId}&mode="add"`;
 
     /* Open the popup appointment form window */
     window.open(popupURL, 'ADD APPOINTMENT', 'width=600,height=550');
-
-
-    // setPopupVisible(true);
-  };
-
-  /**
-   * Handle the user creating the appointment time with patient name
-   */
-  const handleBookAppointment = async () => {
-
-    /* If no selected time or no selected patient, there is an error */
-    if (!selectedTime || !selectedPatient) {
-      alert('Please select a time and a patient.');
-      return;
-    }
-
-    /* Fetch fresh appointments directly from backend */
-    let freshAppointments = [];
-    try {
-      const response = await fetch(`http://localhost:3002/appointments?date=${currentDate}`);
-      freshAppointments = await response.json();
-    } catch (error) {
-      console.error('Error fetching appointments for conflict check:', error);
-      alert('Unable to verify appointment conflicts');
-      return;
-    }
-
-    /* Get the raw minutes since midnight, will make it easier to compare times */
-    const selectedTimeRawMinutes = (parseInt(selectedTime.split(':')[0]) * 60) + parseInt(selectedTime.split(':')[1]);
-
-    let isConflict = false;
-
-    for (const appt of freshAppointments) {
-
-      /* Appointment start for current appt being checked */
-      const apptStart = (parseInt(appt.start_time.slice(0, 2)) * 60) + parseInt(appt.start_time.slice(3, 5));
-
-      /* Appointment end for current appt being checked */
-      const apptEnd = apptStart + appt.duration_minutes;
-
-      /* Appointment start for appointment trying to be booked */
-      const selectedStart = selectedTimeRawMinutes;
-
-      /* Appointment end for appointment trying to be booked */
-      const selectedEnd = selectedStart + duration;
-
-      if ((selectedStart > apptStart) && (selectedStart < apptEnd)) {
-        console.log("Time conflict scenario 1");
-        isConflict = true;
-      }
-
-      if ((selectedEnd > apptStart) && (selectedEnd < apptEnd)) {
-        console.log("Time conflict scenario 2");
-        isConflict = true;
-      }
-
-      if ((selectedStart < apptStart) && (selectedEnd > apptEnd)) {
-        console.log("Time conflict scenario 3");
-        isConflict = true;
-      }
-
-      if ((selectedStart > apptStart) && (selectedEnd < apptEnd)) {
-        console.log("Time conflict scenario 4");
-        isConflict = true;
-      }
-    }
-
-    console.log("isConflict? : "+isConflict);
-
-    if (isConflict) {
-      alert('This time slot is already booked for the selected provider');
-      return;
-    }
-
-    /* If the appointment reason is empty, just make the default reason for the appt a 'General Consult' */
-    if (appointmentReason === '') setAppointmentReason('General Consult');
-
-    const appointmentData = {
-      patientId: selectedPatient.id,
-      providerId: selectedProviderId,
-      date: '2025-06-27', /* Hardcode date for now, make dynamically later */
-      time: selectedTime,
-      duration: duration,
-      reason: appointmentReason,
-      status: 'booked' /* Initial status is the patient is just booked */
-    };
-
-    try {
-      const response = await fetch('http://localhost:3002/appointments', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(appointmentData)
-      });
-
-      const result = await response.json();
-
-      if (!response.ok) {
-        console.error(result.error);
-        alert('Failed to book appointment');
-      } else {
-        console.log('Appointment booked:', result.message);
-        alert('Appointment booked successfully');
-
-        /* Fetch updated appointments */
-        await fetchAppointments();
-
-        /* Clear all the current booking info in the fields */
-        resetBookingState();
-      }
-
-    } catch (error) {
-      console.error('Booking failed', error);
-      alert('Error booking appointment');
-    };
-
-    /* Fix logic later */
-    console.log(`Booked ${patientName} at ${selectedTime}`);
-    setShowBookingWindow(false);
-    setPatientName('');
-  };
-
-  /**
-   * Handle the patient search to call backend API
-   */
-  const handlePatientSearch = async (e) => {
-    const keyword = e.target.value;
-    setPatientName(keyword);
-
-    try {
-      const response = await fetch(`http://localhost:3002/patients/search?keyword=${encodeURIComponent(keyword)}&mode=search_name`);
-      const data = await response.json();
-      console.log('Search API response:', data);
-      setSearchResults(Array.isArray(data) ? data : []);
-    } catch (error) {
-      console.error('Search failed:', error);
-      setSearchResults([]);
-    }
   };
 
   /**
@@ -291,12 +103,6 @@ function MainMenu() {
    * Handle editing an appointment after clicking on a patients name on the timesheet
    */
   const handleEditAppointment = async (time, providerId, appt) => {
-    resetBookingState();
-    setSelectedTime(time);
-    setSelectedProviderId(providerId);
-    // setSelectedAppointment(appt);
-    // setShowBookingWindow(true);
-    console.log("GOT HERE 1");
 
     /* Query string with the info to be passed to the popup window */
     const popupURL = `/appointment-form-popup?time=${encodeURIComponent(time)}&providerId=${providerId}&mode=edit&apptId=${appt.id}`;
@@ -304,44 +110,6 @@ function MainMenu() {
     /* Open the popup appointment form window */
     window.open(popupURL, 'EDIT APPOINTMENT', 'width=600,height=550');
   }
-
-  /**
-   * Handle deleting an appointment after clicking on a patients name on the timesheet
-   */
-  const handleDeleteAppointment = async (apptId) => {
-
-    /* Remove the selected appointment and set it to no selected appt (null) */
-    setSelectedAppointment(null);
-
-    try {
-      const response = await fetch(`http://localhost:3002/appointments/${apptId}`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ id : apptId})
-      });
-
-      const result = await response.json();
-
-      if (!response.ok) {
-        console.error(result.error);
-        alert('Failed to delete appointment');
-      } else {
-        console.log('Deleted appointment');
-      }
-
-      /* Clear all the current booking info in the fields */
-      resetBookingState();
-
-      /* Refresh the appointment list */
-      await fetchAppointments();
-
-    } catch (error) {
-      console.error('Error deleting appointment:', error);
-      alert('Error deleting appointment');
-    }
-  };
 
   /**
    * Generate the 5 minute time slots from 7:00 AM to 23:55 PM for the schedule on given day on the UI
@@ -428,15 +196,6 @@ function MainMenu() {
                     <span
                       className="patient-name"
                       onClick={() => {
-                        setSelectedTime(appt.start_time.slice(0,5));
-                        setSelectedProviderId(appt.provider_id);
-                        setSelectedPatient({
-                          id: appt.patient_id,
-                          firstname: appt.firstname,
-                          lastname: appt.lastname,
-                        });
-                        setSelectedAppointment(appt);
-                        setShowBookingWindow(true);
                         handleEditAppointment(time, 1, appt);
                       }}
                       style = {{ cursor: 'pointer' }}
@@ -516,16 +275,7 @@ function MainMenu() {
                     <span
                       className="patient-name"
                       onClick={() => {
-                        setSelectedTime(appt.start_time.slice(0,5));
-                        setSelectedProviderId(appt.provider_id);
-                        setSelectedPatient({
-                          id: appt.patient_id,
-                          firstname: appt.firstname,
-                          lastname: appt.lastname,
-                        });
-                        setSelectedAppointment(appt);
-                        setShowBookingWindow(true);
-                        handleEditAppointment(time, 2);
+                        handleEditAppointment(time, 2, appt);
                       }}
                       style = {{ cursor: 'pointer' }}
                     >
@@ -603,76 +353,6 @@ function MainMenu() {
           <tbody>{generateTimeRows()}</tbody>
         </table>
       </div>
-
-      {/* Show the popup window when trying to book an appt*/}
-      {/* {showBookingWindow && (
-        <div className="popup-background">
-          <div className="popup">
-            <h2>Book Appointment for {selectedTime}</h2>
-            <label>
-              Duration:
-              <select value={duration} onChange={(e) => setDuration(parseInt(e.target.value))}>
-                <option value={5}>5 minutes</option>
-                <option value={10}>10 minutes</option>
-                <option value={15}>15 minutes</option>
-                <option value={20}>20 minutes</option>
-                <option value={25}>25 minutes</option>
-                <option value={30}>30 minutes</option>
-              </select>
-              Search Patient:
-              <input type="text"
-              value={patientName}
-              onChange={handlePatientSearch}
-              placeholder='Enter patient name'
-              />
-            </label>
-            {selectedPatient && (<p>Selected: {selectedPatient.firstname} {selectedPatient.lastname}</p>)} */}
-            {/* Show the search results in the popup window */}
-            {/* <ul className="search-results">
-              {searchResults.map((patient) => (
-                <li 
-                  key={patient.id}
-                  onClick={() => setSelectedPatient(patient)}
-                  className="search-result-item"
-                >
-                  {patient.lastname}, {patient.firstname}
-                </li>
-              ))}
-            </ul>
-            <label>
-              Reason:
-              <input
-                type="text"
-                value={appointmentReason}
-                onChange={(e) => setAppointmentReason(e.target.value)}
-                placeholder='Give reason for appointment'
-              />
-            </label>
-            <div className="popup-buttons">
-              <button onClick={() => handleBookAppointment()}>Book</button>
-              <button onClick={() => setShowBookingWindow(false)}>Cancel</button>
-              {selectedAppointment && (
-                <button onClick={() => handleDeleteAppointment(selectedAppointment.id)}>Delete</button>
-              )}
-            </div>
-
-          </div>
-        </div>
-      )} */}
-
-      {/* Show the popup window */}
-      {/* {popupVisible && (
-        <AppointmentFormPopup
-          visible={true}
-          mode={'add'}
-          selectedTime={selectedTime}
-
-
-          onClose={() => setPopupVisible(false)}
-
-        />
-      )} */}
-
     </div>
   );
 }
