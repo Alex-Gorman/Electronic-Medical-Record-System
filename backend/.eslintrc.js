2e80e662f343d41b100b2768226c8cf334d5a4d9
Alex Gorman <alexgorman@Alexs-MacBook-Pro.local>
2025-08-06 00:59:37 -0400

    chore(ci): migrate ESLint config to CommonJS .eslintrc.js

---
+++ backend/.eslintrc.js
@@ -0,0 +1,19 @@
+// backend/.eslintrc.js
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  settings: {
+    react: { version: "detect" },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  rules: {
+    /* custom overrides here */
+  },
+};
